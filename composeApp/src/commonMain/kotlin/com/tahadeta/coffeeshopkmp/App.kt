4548b7a8aef06183db78873f68ecc5d0ba51be6b
Taha Jadid <jadid.taha@waroom.com>
2024-10-30 16:20:28 +0100

    Separate Login Screen

--- composeApp/src/commonMain/kotlin/com/tahadeta/coffeeshopkmp/App.kt
+++ composeApp/src/commonMain/kotlin/com/tahadeta/coffeeshopkmp/App.kt
@@ -1,28 +1,8 @@
 package com.tahadeta.coffeeshopkmp
 
-import androidx.compose.animation.animateContentSize
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.unit.dp
-import dev.gitlive.firebase.Firebase
-import dev.gitlive.firebase.auth.FirebaseUser
-import dev.gitlive.firebase.auth.auth
-import kotlinx.coroutines.launch
+import com.tahadeta.coffeeshopkmp.Screen.LoginScreen
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Suppress("ktlint:standard:function-naming")
@@ -30,80 +10,6 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 @Preview
 fun App() {
     MaterialTheme {
-        val scope = rememberCoroutineScope()
-        val auth = remember { Firebase.auth }
-        var firebaseUser: FirebaseUser? by remember { mutableStateOf(null) }
-        var userEmail by remember { mutableStateOf("") }
-        var userPassword by remember { mutableStateOf("") }
-
-        if (firebaseUser == null) {
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally,
-            ) {
-                TextField(
-                    value = userEmail,
-                    onValueChange = { userEmail = it },
-                    placeholder = { Text(text = "Email..") },
-                )
-                Spacer(modifier = Modifier.height(24.dp))
-                TextField(
-                    value = userPassword,
-                    onValueChange = { userPassword = it },
-                    placeholder = { Text(text = "Password..") },
-                    visualTransformation = PasswordVisualTransformation(),
-                )
-                Spacer(modifier = Modifier.height(24.dp))
-                Button(
-                    onClick = {
-                        scope.launch {
-                            try {
-                                auth.createUserWithEmailAndPassword(
-                                    email = userEmail,
-                                    password = userPassword,
-                                )
-                            } catch (e: Exception) {
-                                auth.signInWithEmailAndPassword(
-                                    email = userEmail,
-                                    password = userPassword,
-                                )
-                            }
-                        }
-                    },
-                ) {
-                    Text(text = "Sign in")
-                }
-
-                Box(
-                    modifier =
-                        Modifier
-                            .background(Color.Red)
-                            .animateContentSize()
-                            .height(400.dp)
-                            .fillMaxWidth(),
-                ) {
-                }
-            }
-        } else {
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally,
-            ) {
-                Text(text = firebaseUser?.uid ?: "Unknown ID")
-                Spacer(modifier = Modifier.height(24.dp))
-                Button(
-                    onClick = {
-                        scope.launch {
-                            auth.signOut()
-                            firebaseUser = auth.currentUser
-                        }
-                    },
-                ) {
-                    Text(text = "Sign out")
-                }
-            }
-        }
+        LoginScreen()
     }
 }
